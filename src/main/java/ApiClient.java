8525f0fef3d163998fcd2e5c2ab761d5db1e0f45
Joshy <117656288+Joshy271@users.noreply.github.com>
2023-10-31 14:47:32 -0230

    Got spring boot working

--- src/main/java/ApiClient.java
+++ src/main/java/ApiClient.java
@@ -1,2 +1,26 @@
-public class Client {
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+public class ApiClient {
+    private final String baseUrl;
+
+    public ApiClient(String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    public String getItems() throws IOException {
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpGet httpGet = new HttpGet(baseUrl + "/items");
+            HttpResponse response = httpClient.execute(httpGet);
+            return EntityUtils.toString(response.getEntity());
+        }
+    }
+
+    // Other API methods for POST, PUT, DELETE can be implemented here
 }
+
